84ee450624f85feb5fc6a759764699f7c0cd1dde
Kevin <fitz0017@users.noreply.github.com>
2023-04-17 00:25:59 -0400

    initial commit

---
+++ mapUpdate.js
@@ -0,0 +1,44 @@
+const url = "https://api-v3.mbta.com/vehicles?filter[route]=1&include=trip";
+
+const markers = [];
+// Pass in API Key with the request using the header x-api-key
+async function load() {
+  const locations = await getBusLocations();
+  console.log(new Date());
+  console.log(locations);
+
+  for (let i = 0; i < locations.length; i++) {
+    let latlng = new google.maps.LatLng(
+      locations[i]["attributes"]["latitude"],
+      locations[i]["attributes"]["longitude"]
+    );
+    let marker = new google.maps.Marker({
+      position: latlng,
+      title: `Bus ${i}`,
+    });
+    markers.push(marker);
+  }
+}
+
+async function getBusLocations() {
+  const res = await fetch(url);
+  const busses = await res.json();
+  return busses.data;
+}
+
+async function move() {
+  const updatedLocations = await getBusLocations();
+  for (let i = 0; i < updatedLocations.length; i++) {
+    markers[i].setMap(null);
+    let latlng = new google.maps.LatLng(
+      updatedLocations[i]["attributes"]["latitude"],
+      updatedLocations[i]["attributes"]["longitude"]
+    );
+    markers[i]["position"] = latlng;
+    markers[i].setMap(map);
+  }
+  setTimeout(move, 15000);
+}
+
+// load();
+// move();

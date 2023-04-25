b2cccbf5324788ebefd28f6b853dbee3b54668d6
Kevin <fitz0017@users.noreply.github.com>
2023-04-25 14:57:29 -0400

    cleans up tomorrow js file

--- tomorrow.js
+++ tomorrow.js
@@ -1,5 +1,5 @@
 const tomorrowUrl =
-  "https://api.tomorrow.io/v4/timelines?location=41.230698,-73.064034&fields=temperature&timesteps=1h&units=metric&apikey=ognXDdNVoElqST4Uok8jClJ6XW9EGThB";
+  "https://api.tomorrow.io/v4/timelines?location=41.230698,-73.064034&fields=temperature&timesteps=1h&units=metric&apikey=";
 var lat = ``;
 var long = ``;
 tomorrowKey = ``;

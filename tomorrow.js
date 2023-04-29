cb2e1648e426dbad69af4f87e7aa68a7021c1fd0
Kevin <fitz0017@users.noreply.github.com>
2023-04-29 10:34:31 -0400

    corrects tomorrow api url

--- tomorrow.js
+++ tomorrow.js
@@ -2,10 +2,10 @@ const tomorrowUrl =
   "https://api.tomorrow.io/v4/timelines?location=41.230698,-73.064034&fields=temperature&timesteps=1h&units=metric&apikey=";
 var lat = ``;
 var long = ``;
-tomorrowKey = ``;
-fields = "temperature&timesteps=1hr&units=metric";
+var tomorrowKey = config.tomorrowKey;
+var fields = "temperature&timesteps=1hr&units=metric";
 
-async function getWeather() {
+async function getWeather(tomorrowUrl, fields, location, apiKey) {
   let url = `${tomorrowUrl}&location=${location}&fields=${fields}apiKey=${apiKey}`;
   const wx = await fetch(url);
 }
